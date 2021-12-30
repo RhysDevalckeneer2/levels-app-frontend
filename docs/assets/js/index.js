f19457a6dbe336775465f7dd69b1d4e94160b616
Rhys Devalckeneer <44397726+RhysDevalckeneer2@users.noreply.github.com>
2021-12-30 22:17:03 +0100

    added feature js

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -16,7 +16,7 @@
   \**********************/
 /***/ (() => {
 
-eval("// index.js\n\n\nconsole.log('Index.js loaded!');\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("// index.js\n\n\nconsole.log('Index.js loaded!');\n\nwindow.onload = function() {\n\n    let users = [];\n\n    document.getElementById('signup-form').addEventListener('submit', event => {\n        event.preventDefault()\n        console.log('Submit clicked!')\n\n        const user = {\n            username: this.name,\n            fullname: this.fullname,\n            email: this.email,\n            course: this.course\n        };\n\n        user.username = document.getElementById('username').value;\n        user.fullname = document.getElementById('fullname').value;\n        user.email = document.getElementById('email').value;\n        user.course = document.getElementById('course').value;\n\n        users.push(user)\n        console.log(users)\n    })\n}\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -2,3 +2,29 @@
 'use strict';
 
 console.log('Index.js loaded!');
+
+window.onload = function() {
+
+    let users = [];
+
+    document.getElementById('signup-form').addEventListener('submit', event => {
+        event.preventDefault()
+        console.log('Submit clicked!')
+
+        const user = {
+            username: this.name,
+            fullname: this.fullname,
+            email: this.email,
+            course: this.course
+        };
+
+        user.username = document.getElementById('username').value;
+        user.fullname = document.getElementById('fullname').value;
+        user.email = document.getElementById('email').value;
+        user.course = document.getElementById('course').value;
+
+        users.push(user)
+        console.log(users)
+    })
+}
+
